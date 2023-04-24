48f5301340a78f00540a3dbfee94cd8c15ca6b97
Ali Enrique Briceño <alienriquebm@gmail.com>
2023-04-23 22:50:13 -0400

    button and input created

--- components/UI/Button/Button.tsx
+++ components/UI/Button/Button.tsx
@@ -1,3 +1,4 @@
+import { InputLoadingIcon } from "@/assets/icons";
 import Link from "next/link";
 import React from "react";
 
@@ -5,6 +6,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   leadingIcon?: React.ReactNode;
   trailingIcon?: React.ReactNode;
+  loading?: boolean;
   href?: string;
 }
 
@@ -13,12 +15,14 @@ const Button = ({
   href,
   leadingIcon,
   trailingIcon,
+  loading,
   ...props
 }: ButtonProps) => {
   if (href) {
     return (
       <Link href={href}>
         <button
+          disabled={loading || props.disabled}
           className="flex min-w-[114px] items-center justify-items-center rounded bg-blue-500 py-2 px-3 text-sm text-white duration-300 ease-in-out hover:bg-blue-300 hover:duration-300 hover:ease-in-out focus:border-blue-900 active:bg-blue-900 active:duration-300 active:ease-in-out disabled:bg-gray-300"
           {...props}
         >
@@ -32,12 +36,18 @@ const Button = ({
 
   return (
     <button
+      disabled={loading || props.disabled}
       className="flex min-w-[114px] items-center justify-items-center rounded bg-blue-500 py-2 px-3 text-sm text-white duration-300 ease-in-out hover:bg-blue-300 hover:duration-300 hover:ease-in-out focus:border-blue-900 active:bg-blue-900 active:duration-300 active:ease-in-out disabled:bg-gray-300"
       {...props}
     >
       {leadingIcon && <span className="mr-2">{leadingIcon}</span>}
       <span className="w-full">{children}</span>
-      {trailingIcon && <span className="ml-2">{trailingIcon}</span>}
+      {trailingIcon && !loading && <span className="ml-2">{trailingIcon}</span>}
+      {loading && (
+        <span className="ml-2">
+          <InputLoadingIcon />
+        </span>
+      )}
     </button>
   );
 };
